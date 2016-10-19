0ceeabc38bfb71cffdca352f3ee42c0c07a95268
Slipp Douglas Thompson <git@capnslipp.com>
2016-10-19 18:47:47 -0500

    WIP Added module.modulemap

---
+++ Finch/Finch.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import <AVFoundation/AVFoundation.h>
+#import <OpenAL/al.h>
+#import <OpenAL/alc.h>
+
+#import "FISound.h"
+#import "FISoundEngine.h"
